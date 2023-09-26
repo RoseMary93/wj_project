a36f8d0470235fe5e2d049b1137136825db89b81
z9960326 <z9960326@gmail.com>
2023-09-26 16:20:46 +0800

    flask 20230926

---
+++ python_jupyter/Flask/static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        alert('Submit!');
+    }); 
+});
