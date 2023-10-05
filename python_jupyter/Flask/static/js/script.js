da3592b5fe92a70510c784a7b454b5d0030cd948
z9960326 <z9960326@gmail.com>
2023-10-06 02:03:38 +0800

    delete unsless folder 20231006

--- python_jupyter/Flask/static/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var form = document.querySelector('form');
-    form.addEventListener('submit', function(event) {
-        alert('Submit!');
-    }); 
-});
